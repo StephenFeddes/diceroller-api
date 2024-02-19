f190efa9764ed3e7d4f0cd66b0c706bcf4023111
StephenFeddes <feddessteve@gmail.com>
2024-02-19 11:57:55 -0600

    Init commit.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+require("dotenv").config();
+const API_URL = process.env.API_URL;
+
+// Define a route to serve your HTML file
+app.get("/", (req, res) => {
+    // Send the HTML file with the dynamically created JavaScript file
+    res.sendFile(__dirname + "/index.html");
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get("/roll", (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    res.send(`${randomNumber}`);
+});
