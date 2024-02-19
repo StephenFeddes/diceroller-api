a3116a4bb765ac351d56df42ef59d6d4b4db6998
StephenFeddes <feddessteve@gmail.com>
2024-02-19 12:26:31 -0600

    Testing CI/CD

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# diceroller-api
+# Dice Roller API
+API for Stephen Feddes's dice rolling game
+All work is original.
+School email: stephenpfeddes@lewisu.edu
--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-require("dotenv").config();
-const API_URL = process.env.API_URL;
 
 // Define a route to serve your HTML file
 app.get("/", (req, res) => {
     // Send the HTML file with the dynamically created JavaScript file
-    res.sendFile(__dirname + "/index.html");
+    //res.sendFile(__dirname + "/index.html");
+    console.log("Dice roller API")
 });
 
 // Start the server
