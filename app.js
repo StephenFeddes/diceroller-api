a19c27aac9722f7b88f4c257f13f995b2db16818
StephenFeddes <feddessteve@gmail.com>
2024-02-19 21:34:51 -0600

    Testing CI/CD.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
+const cors = require('cors');
 
-// Define the directory for static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route to serve your HTML file
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/roll", (req, res) => {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-    res.json(randomNumber);
+app.get('/roll', (req, res) => {
+    const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    res.json([randomNumber1, randomNumber2]);
 });
 
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
